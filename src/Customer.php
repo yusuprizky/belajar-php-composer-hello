48a5b252630e7b3baa101cd881ffe74c6b3a1d09
Yusup Rizky Rinaldy <yusuprizkyr29@gmail.com>
2022-12-04 13:01:04 +0700

    add default value, change name of namespace

--- src/Customer.php
+++ src/Customer.php
@@ -1,6 +1,6 @@
 <?php 
 
-namespace ProgrammerZamanNow\Belajar;
+namespace Yrr\Belajar;
 
 class Customer
 {
@@ -9,7 +9,7 @@ class Customer
         
     }
 
-    public function sayHello(string $name): string
+    public function sayHello(string $name = "Guest"): string
     {
         return "Hello $name, My name is $this->name";
     }
